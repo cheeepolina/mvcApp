4abcaac395fabd4dc0a9e0ada749255ad20d58da
cheeepolina <trehlebp@gmail.com>
2023-06-14 11:21:32 +0300

    mvc3,4

---
+++ mvc3/src/main/java/com/example/buysell/controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.example.buysell.controllers;
+
+import com.example.buysell.models.User;
+import com.example.buysell.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.buf.UDecoder;
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+
+    @PostMapping("/registration")
+    public String createUser(User user) {
+        userService.createUser(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/hello")
+    public String securityUrl() {
+        return "hello";
+    }
+
+    @GetMapping("/user/{user}")
+    public String userInfo(@PathVariable("user") User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "user-info";
+
+    }
+}
---
+++ mvc3/src/main/java/com/example/buysell/services/UserService.java
@@ -0,0 +1,43 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.User;
+import com.example.buysell.models.enums.Role;
+import com.example.buysell.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean createUser(User user) {
+        String userEmail = user.getEmail();
+        if (userRepository.findByEmail(userEmail) != null) return false;
+        user.setActive(true);
+        user.getRoles().add(Role.ROLE_ADMIN);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        log.info("Saving new User with email: {}", userEmail);
+        userRepository.save(user);
+        return true;
+    }
+
+    public List<User> list(){
+        return userRepository.findAll();
+    }
+
+    public void banUser(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if(user!=null){
+            user.setActive(false);
+            log.info("Ban user with id ={}; email: {}", user.getId(), user.getEmail());
+        }
+        userRepository.save(user);
+    }
+}
